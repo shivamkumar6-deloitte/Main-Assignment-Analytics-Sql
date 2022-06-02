6ee819336f0d8e43fbeefb03cd28e6869462cc77
shivamkumar6 <shivamkumar6@deloitte.com>
2022-06-02 14:17:45 +0530

    Documentation Added

--- MainAssignment/models/example/milestone1_1.sql
+++ MainAssignment/models/example/milestone1_1.sql
@@ -1,2 +1,4 @@
 
-select  code,avg(nav) as MonthlyNav,avg(repurchase_price) as repurchase_average, avg(sale_price) as sale_average  from "ANALYTICS"."MAINASSIGNEMT"."navhistory"  group by code, EXTRACT( MONTH FROM nav_date)  
+select  code,avg(nav) as MonthlyNav,avg(repurchase_price) as repurchase_average, avg(sale_price) as sale_average  
+from "ANALYTICS"."MAINASSIGNEMT"."navhistory"  
+group by code, EXTRACT( MONTH FROM nav_date)  
--- MainAssignment/models/example/timespan.sql
+++ MainAssignment/models/example/timespan.sql
@@ -1,3 +1,5 @@
 {{ config(materialized='view') }}
 
-select na.code, mu.name, max(na.nav_date) as lastdate , min(na.nav_date) as firstdate from "ANALYTICS"."MAINASSIGNEMT"."navhistory" na, "ANALYTICS"."MAINASSIGNEMT"."mutualfund" mu where year(nav_date) = 2018 and na.code=mu.code group by na.code, mu.name 
+select na.code, mu.name, max(na.nav_date) as lastdate , min(na.nav_date) as firstdate 
+from "ANALYTICS"."MAINASSIGNEMT"."navhistory" na, "ANALYTICS"."MAINASSIGNEMT"."mutualfund" mu 
+where year(nav_date) = 2018 and na.code=mu.code group by na.code, mu.name 
