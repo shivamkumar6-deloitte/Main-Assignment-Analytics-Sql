d56fd6a9aa9f31f4314ae8faff00ecfeb3fae4d4
shivamkumar6 <shivamkumar6@deloitte.com>
2022-06-02 08:29:19 +0530

    fixed a few bugs

--- MainAssignment/models/example/milestone2_4.sql
+++ MainAssignment/models/example/milestone2_4.sql
@@ -1,3 +1,3 @@
 select q2.code, q1.name ,  (q1.lastNav - q2.firstNav) as Performance  from
 (select  na.code,y.name, na.nav as lastNav  from {{ ref('liquid') }} y, "ANALYTICS"."MAINASSIGNEMT"."navhistory" na where na.nav_date = y.lastdate and y.code = na.code) q1 inner join 
-(select  na.code,y.name, na.nav as FirstNav  from {{ ref('liquid') }} y, "ANALYTICS"."MAINASSIGNEMT"."navhistory" na where na.nav_date = y.firstdate and y.code = na.code) q2 on (q1.code = q2.code);  
+(select  na.code,y.name, na.nav as FirstNav  from {{ ref('liquid') }} y, "ANALYTICS"."MAINASSIGNEMT"."navhistory" na where na.nav_date = y.firstdate and y.code = na.code) q2 on (q1.code = q2.code)
