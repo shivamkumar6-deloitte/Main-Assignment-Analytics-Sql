a254686fc2477d55559bb77b910600c85537ea71
shivamkumar6 <shivamkumar6@deloitte.com>
2022-06-02 08:27:11 +0530

    fixed a few bugs

--- MainAssignment/models/example/liquid.sql
+++ MainAssignment/models/example/liquid.sql
@@ -2,4 +2,4 @@
 
 select na.code, mu.name, max(na.nav_date) as lastdate , min(na.nav_date) as firstdate 
 from "ANALYTICS"."MAINASSIGNEMT"."navhistory" na, "ANALYTICS"."MAINASSIGNEMT"."mutualfund" mu 
-where year(nav_date) = 2018 and mu.category_id=2 and na.code=mu.code group by na.code, mu.name ;
+where year(nav_date) = 2018 and mu.category_id=2 and na.code=mu.code group by na.code, mu.name 
